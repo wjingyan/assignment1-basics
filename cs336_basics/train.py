63f5c618fa7d7f85dc6c8976074980fd7968f726
wjingyan <freecady@gmail.com>
2026-05-04 18:05:50 -0700

    data loading

--- cs336_basics/train.py
+++ cs336_basics/train.py
@@ -117,6 +117,21 @@ def load_data(x, batch_size, context_length, device):
     targets = torch.stack([torch.tensor(x[i+1:i+context_length+1], dtype=torch.long) for i in start_indices])
     return inputs.to(device), targets.to(device)
 
+def save_checkpoint(model: torch.nn.Module, optimizer: torch.optim.Optimizer, iteration: int, out: str | os.PathLike | typing.BinaryIO | typing.IO[bytes]):
+    model_state = model.state_dict()
+    optimizer_state = optimizer.state_dict()
+    checkpoint = {
+        "model_state": model_state,
+        "optimizer_state": optimizer_state,
+        "iteration": iteration,
+    }
+    torch.save(checkpoint, out)
+
+def load_checkpoint(src: str | os.PathLike | typing.BinaryIO | typing.IO[bytes], model: torch.nn.Module, optimizer: torch.optim.Optimizer):
+    checkpoint = torch.load(src)
+    model.load_state_dict(checkpoint["model_state"])
+    optimizer.load_state_dict(checkpoint["optimizer_state"])
+    return checkpoint["iteration"]
 
 if __name__ == "__main__":
     simple_training_loop()
--- tests/adapters.py
+++ tests/adapters.py
@@ -581,7 +581,8 @@ def run_save_checkpoint(
             we've completed.
         out (str | os.PathLike | BinaryIO | IO[bytes]): Path or file-like object to serialize the model, optimizer, and iteration to.
     """
-    raise NotImplementedError
+    from cs336_basics.train import save_checkpoint
+    save_checkpoint(model, optimizer, iteration, out)
 
 
 def run_load_checkpoint(
@@ -602,7 +603,8 @@ def run_load_checkpoint(
     Returns:
         int: the previously-serialized number of iterations.
     """
-    raise NotImplementedError
+    from cs336_basics.train import load_checkpoint
+    load_checkpoint(model, optimizer, iteration, out)
 
 
 def get_tokenizer(
