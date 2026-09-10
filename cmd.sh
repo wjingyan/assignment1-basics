5c3e17bbaa0e20b33dfe76497653271523057afb
wjingyan <freecady@gmail.com>
2026-09-09 19:16:50 -0700

    pin cuda version

--- cmd.sh
+++ cmd.sh
@@ -1291,3 +1291,25 @@ uv run cs336_basics/train.py \
     --checkpoint-dir checkpoints/test \
     --save-interval 5000 \
     --lr-max 1.5e-3 --lr-min 1.5e-4 --cosine-cycle-iters 2600
+# Moe cuda 1 batch test
+uv run cs336_basics/train.py \
+    --train-data output/encoding/encoded_owt_train.npy \
+    --val-data output/encoding/encoded_owt_valid.npy \
+    --vocab-size 32000 \
+    --context-length 256 \
+    --d-model 1024 \
+    --num-layers 8 \
+    --num-heads 16 \
+    --d-ff 2730 \
+    --num-experts 8 \
+    --moe-coef 0.01 \
+    --batch-size 64 \
+    --max-iters 500 \
+    --warmup-iters 100 \
+    --device cuda \
+    --checkpoint-dir checkpoints/test \
+    --save-interval 5000 \
+    --wandb-project cs336-basics \
+    --wandb-run-name moe-smoketest \
+    --lr-max 1.5e-3 --lr-min 1.5e-4 --cosine-cycle-iters 2600
+runpodctl stop pod $RUNPOD_POD_ID
